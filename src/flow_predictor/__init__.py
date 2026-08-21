73c8b8cc374edf20d0a451f1788aae8b81d31a34
Elliot <hack00mind@gmail.com>
2026-08-21 23:04:25 +0800

    feat: prepare fake data

--- src/flow_predictor/__init__.py
+++ src/flow_predictor/__init__.py
@@ -1,12 +1,14 @@
 from typing import Annotated
 import typer
+from flow_predictor.fake import fake_all
 
 app = typer.Typer()
 
 @app.command()
 def cli(fake: Annotated[bool, typer.Option(help="Generate fake data.")] = False, train: Annotated[bool, typer.Option(help="Start training.")] = False):
     if fake:
-        print("fake")
+        df = fake_all()
+        print(f"Fake data: {df.describe()}")
     if train:
         print("train")
 
